f405898b7e99ffef8046ae71f6cf1bdc343979ea
NEDELCU Valeriu <valeriu.nedelcu@ext.ec.europa.eu>
2022-10-25 09:38:20 +0200

    Added method for handling HTTP PUT for BeerDto (updating a beer)

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -30,4 +30,12 @@ class BreweryClientTest {
         assertNotNull(uri);
         System.out.println("uri = " + uri);
     }
+
+    @Test
+    void testUpdateBeer() {
+        //given
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        client.updateBeer(UUID.randomUUID(), beerDto);
+    }
 }
