e83d951271595f0dbb5b61c880329720b4f547eb
NEDELCU Valeriu <valeriu.nedelcu@ext.ec.europa.eu>
2022-10-25 18:44:50 +0200

    added externalized configuration for http client

--- src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -5,7 +5,10 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
@@ -17,15 +20,43 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
 
-    public ClientHttpRequestFactory clientHttpRequestFactory(){
+    @ConfigurationProperties(prefix = "apache.http.client")
+    @Configuration
+    static class ApacheHttpClientConfig {
+        int maxTotal;
+        int defaultMaxPerRoute;
+        int connectionRequestTimeout;
+        int socketTimeout;
+
+        public void setMaxTotal(int maxTotal) {
+            this.maxTotal = maxTotal;
+        }
+
+        public void setDefaultMaxPerRoute(int defaultMaxPerRoute) {
+            this.defaultMaxPerRoute = defaultMaxPerRoute;
+        }
+
+        public void setConnectionRequestTimeout(int connectionRequestTimeout) {
+            this.connectionRequestTimeout = connectionRequestTimeout;
+        }
+
+        public void setSocketTimeout(int socketTimeout) {
+            this.socketTimeout = socketTimeout;
+        }
+    }
+
+    @Autowired
+    ApacheHttpClientConfig httpClientConfig;
+
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100);
-        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setMaxTotal(httpClientConfig.maxTotal);
+        connectionManager.setDefaultMaxPerRoute(httpClientConfig.defaultMaxPerRoute);
 
         RequestConfig requestConfig = RequestConfig
                 .custom()
-                .setConnectionRequestTimeout(3000)
-                .setSocketTimeout(3000)
+                .setConnectionRequestTimeout(httpClientConfig.connectionRequestTimeout)
+                .setSocketTimeout(httpClientConfig.socketTimeout)
                 .build();
 
         CloseableHttpClient httpClient = HttpClients
